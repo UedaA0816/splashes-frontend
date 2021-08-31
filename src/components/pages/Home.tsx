b2c7d09d472477f5d490ed70c3e1dec5479037c9
ueda <>
2021-08-31 07:49:17 +0000

    fix template

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import Header from "../organisms/Header";
+import Unsplash from "../organisms/Unsplash";
 import HeaderMain from "../templates/HeaderMain";
 
 const Home = ()=>{
   return (
   <HeaderMain>
     <Header />
-    <div>a</div>
-    <div>a</div>
+    <Unsplash />
   </HeaderMain>
   )
 }
--- src/components/templates/HeaderMain.tsx
+++ src/components/templates/HeaderMain.tsx
@@ -3,9 +3,9 @@ import React from "react";
 const HeaderMain:React.FC<{}> = ({children})=>{
   const [header,...main] = React.Children.toArray(children)
   return (
-    <div>
+    <div className="h-full flex flex-col">
       <header>{header}</header>
-      {main.map((v)=><main>{v}</main>)}
+      {main.map((v)=><main className="flex-grow">{v}</main>)}
     </div>
   )
 }
