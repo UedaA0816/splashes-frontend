f2aa8eaa903e683a7e2d655bba5ea441e894b408
ueda <>
2021-08-31 18:36:18 +0900

    add Button

---
+++ src/components/molecules/Button.tsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+const Button = styled.button`
+background: #3DB46D;
+box-shadow: 0px 1px 6px rgba(0, 0, 0, 0.1);
+border-radius: 12px;
+padding:16px 20px 17px;
+
+font-weight: bold;
+font-size: 16px;
+line-height: 22px;
+color: #FFFFFF;
+`
+
+export default Button
