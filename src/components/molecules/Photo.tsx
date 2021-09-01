2b0c8f01c89940c4cc4c33b55d32a74460f98957
ueda <>
2021-09-01 18:43:14 +0900

    fix photo hover

--- src/components/molecules/Photo.tsx
+++ src/components/molecules/Photo.tsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PhotoType from '../../types/Photo'
 
 type PhotoProps = PhotoType
 
 const Photo:React.VFC<PhotoProps> = ({label,photo_uri})=>{
-  return (<div className="">
-    <img src={photo_uri} alt="" loading="lazy" className="w-full" />
-  </div>)
-} 
+  const [hover, setHover] = useState(false)
+  const handlerMouseEnter:React.MouseEventHandler<HTMLDivElement> = (e) => {
+    setHover(true)
+  }
+  const handlerMouseLeave:React.MouseEventHandler<HTMLDivElement> = (e) => {
+    setHover(false)
+  }
+  const className = `w-full rounded-2xl transition ${hover ? "filter brightness-75" : ""}`
+  const textClassName = `text-white absolute bottom-0 w-full px-4 mb-2 font-bold text-lg opacity-0 transition-opacity ${hover?"opacity-100":""}`
+  return (
+    <div className="rounded-2xl relative" onMouseEnter={handlerMouseEnter} onMouseLeave={handlerMouseLeave}>
+      <img src={photo_uri} alt="" loading="lazy" className={className} />
+      {
+        <span className={textClassName}>{label}</span>
+      }
+    </div>
+  )
+}
 export default Photo
